db80d52e8c6dbb269eabbaf76ecefcfa7406db11
AilinCaminos <ailin.m.caminos@gmail.com>
2020-06-25 20:47:46 -0300

    Se crean Repos Persona, Usuario y Billetera, a medida que se avance con la logica de negocio se verificara que metodos se puedan necesitar y se le agrega la anotacion @Repository para identificarlo. Se crea Services Persona, Usuario y Billetera, y se empiezan a pensar y anotar metodos haciendo un bosquejo

---
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/services/BilleteraService.java
@@ -0,0 +1,42 @@
+package ar.com.ada.api.billeteravirtual.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BilleteraService {
+
+    
+    /**
+     * Metodo cargarSaldo 
+     * buscar billetera por id 
+     * se identifica cuenta por moneda
+     * determinar importe a cargar 
+     * hacer transaccion
+     * 
+     * ver delegaciones sobre entidades
+     * 
+     */
+
+    /**
+     * Metodo enviarSaldo 
+     * buscar billetera por id 
+     * se identifica cuenta por moneda
+     * determinar importe a transferir
+     * billetera de origen y billetera destino
+     * actualizar los saldos de las cuentas (resta en la origen y suma en la destino)
+     * generar 2 transacciones
+     * 
+     * ver delegaciones sobre entidades
+     * 
+     */
+
+     /**
+      * Metodo consultarSaldo
+      * buscar billetera por id
+      * se identifica cuenta por moneda
+      * traer saldo
+      */
+
+
+     
+}
---
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/services/UsuarioService.java
@@ -0,0 +1,19 @@
+package ar.com.ada.api.billeteravirtual.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    
+    /**Metodo para crearUsuario
+     * 1 crear persona (se le settea un usuario)
+     * 2 crear usuario
+     * 3 crear billetera 
+     * 4 crear cuenta por moneda(ARS y/o USD?)
+     */
+
+     /**Metodo IniciarSesion
+      * recibe usuario y contraseña
+      * validar usuario y contraseña
+      */
+}
