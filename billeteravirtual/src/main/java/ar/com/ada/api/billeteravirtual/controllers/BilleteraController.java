4021551edd884d53f1096aa886df15a6e85a4d5a
AilinCaminos <ailin.m.caminos@gmail.com>
2020-08-10 20:09:53 -0300

    Autorizacion de Requests de billetera para checkear si la billetera que esta accediendo le pertenece: Se armaron 2 formas de verificacion, una buscando la data del usuario en base al Principal y la otra ejecutando un Spring Expression Language con PreAuthorize.

--- billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/controllers/BilleteraController.java
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/controllers/BilleteraController.java
@@ -1,9 +1,11 @@
 package ar.com.ada.api.billeteravirtual.controllers;
 
+import java.security.Principal;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ar.com.ada.api.billeteravirtual.entities.Billetera;
 import ar.com.ada.api.billeteravirtual.entities.Cuenta;
 import ar.com.ada.api.billeteravirtual.entities.Transaccion;
+import ar.com.ada.api.billeteravirtual.entities.Usuario;
 import ar.com.ada.api.billeteravirtual.entities.Transaccion.ResultadoTransaccionEnum;
 import ar.com.ada.api.billeteravirtual.models.request.CargaRequest;
 import ar.com.ada.api.billeteravirtual.models.request.EnvioSaldoRequest;
@@ -30,18 +33,24 @@ public class BilleteraController {
     @Autowired
     UsuarioService usuarioService;
 
-    /*
-     * webMetodo 1: consultar saldo: GET URL:/billeteras/{id}/saldos
-     * URL:/billeteras/{id}/saldos/{moneda} 
-     * webMetodo 2: cargar saldo: POST
-     * URL:/billeteras/{id}/recargas requestBody: { "moneda": "importe": } 
-     * webMetodo 3:
-     *      * enviar saldo: POST URL:/billetera/{id}/envios requestBody: { "moneda":
-     * "importe": "email": "motivo": "detalleDelMotivo": }
-     */
-
+    // Metodo de verificacion 1 del checkeo que el usuario que consulta sea el que
+    // corresponde
+    // Usamos el "Principal": que es una abstraccion que nos permite acceder al
+    // usuario que esta logueado
     @GetMapping("/billeteras/{id}/saldos/{moneda}")
-    public ResponseEntity<?> consultarSaldo(@PathVariable Integer id, @PathVariable String moneda) {
+    public ResponseEntity<?> consultarSaldo(Principal principal, @PathVariable Integer id,
+            @PathVariable String moneda) {
+
+        // Obtengo primero el usuario en base al Principal
+        Usuario usuarioLogueado = usuarioService.buscarPorUsername(principal.getName());
+        // Checkqueo si la billetera le corresponde, si no, 403 Forbiden
+        // Esto deberia hacerlo en cada metodo
+        if (!usuarioLogueado.getPersona().getBilletera().getBilleteraId().equals(id)) {
+            // Generar una alerta de cyberseguridad
+
+            // Es responder un resultado mentiroso: ej 404
+            return ResponseEntity.status(403).build();// Forbideen
+        }
 
         SaldoResponse saldo = new SaldoResponse();
 
@@ -49,9 +58,17 @@ public class BilleteraController {
         saldo.moneda = moneda;
 
         return ResponseEntity.ok(saldo);
+
     }
 
+    // Metodo Verificacion Billetera 2: haciendo lo mismo que antes, pero usando
+    // Spring Expression LANGUAGE(magic)
+    // Aca el principal es el User, este principal no es el mismo principal del
+    // metodo anterior
+    // pero apunta a uno parecido(el de arriba es el principal authentication)
+    // https://docs.spring.io/spring-security/site/docs/3.0.x/reference/el-access.html
     @GetMapping("/billeteras/{id}/saldos")
+    @PreAuthorize("@usuarioService.buscarPorUsername(principal.getUsername()).getPersona().getBilletera().getBilleteraId().equals(#id)")
     public ResponseEntity<List<SaldoResponse>> consultarSaldo(@PathVariable Integer id) {
 
         Billetera billetera = new Billetera();
@@ -71,11 +88,8 @@ public class BilleteraController {
         return ResponseEntity.ok(saldos);
     }
 
-    /**
-     * webMetodo 2: cargar saldo: POST URL:/billeteras/{id}/recargas requestBody: {
-     * "moneda": "importe": } webMetodo
-     */
     @PostMapping("/billeteras/{id}/recargas")
+    @PreAuthorize("@usuarioService.buscarPorUsername(principal.getUsername()).getPersona().getBilletera().getBilleteraId().equals(#id)")
     public ResponseEntity<TransaccionResponse> cargarSaldo(@PathVariable Integer id,
             @RequestBody CargaRequest recarga) {
 
@@ -90,12 +104,8 @@ public class BilleteraController {
 
     }
 
-    /***
-     * enviar saldo: POST URL:/billeteras/{id}/envios requestBody: { "moneda":
-     * "importe": "email": "motivo": "detalleDelMotivo": }
-     */
-
     @PostMapping("/billeteras/{id}/envios")
+    @PreAuthorize("@usuarioService.buscarPorUsername(principal.getUsername()).getPersona().getBilletera().getBilleteraId().equals(#id)")
     public ResponseEntity<TransaccionResponse> enviarSaldo(@PathVariable Integer id,
             @RequestBody EnvioSaldoRequest envio) {
 
@@ -117,7 +127,9 @@ public class BilleteraController {
     }
 
     @GetMapping("/billeteras/{id}/movimientos/{moneda}")
-    public ResponseEntity<List<MovimientosResponse>> consultarMovimientos(@PathVariable Integer id, @PathVariable String moneda){
+    @PreAuthorize("@usuarioService.buscarPorUsername(principal.getUsername()).getPersona().getBilletera().getBilleteraId().equals(#id)")
+    public ResponseEntity<List<MovimientosResponse>> consultarMovimientos(@PathVariable Integer id,
+            @PathVariable String moneda) {
 
         Billetera billetera = new Billetera();
         billetera = billeteraService.buscarPorId(id);
@@ -140,8 +152,10 @@ public class BilleteraController {
         }
         return ResponseEntity.ok(res);
     }
-@GetMapping("/billeteras/{id}/movimientos")
-    public ResponseEntity<List<MovimientosResponse>> consultarMovimientos(@PathVariable Integer id){
+
+    @GetMapping("/billeteras/{id}/movimientos")
+    @PreAuthorize("@usuarioService.buscarPorUsername(principal.getUsername()).getPersona().getBilletera().getBilleteraId().equals(#id)")
+    public ResponseEntity<List<MovimientosResponse>> consultarMovimientos(@PathVariable Integer id) {
 
         Billetera billetera = new Billetera();
         billetera = billeteraService.buscarPorId(id);
@@ -164,7 +178,5 @@ public class BilleteraController {
         }
         return ResponseEntity.ok(res);
     }
-
-
 
 }
